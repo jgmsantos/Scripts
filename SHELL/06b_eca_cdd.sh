17ba696c289cd240077ab485a2ffdfa35cd95533
Guilherme Martins <39347107+jgmsantos@users.noreply.github.com>
2020-07-14 14:23:37 -0300

    Script que calcula o índice eca_ddd como descrito no cabeçalho do script.

--- SHELL/06b_eca_cdd.sh
+++ SHELL/06b_eca_cdd.sh
@@ -28,29 +28,28 @@ ANOF="2020"
 for ano in $(seq ${ANOI} ${ANOF})
 do
     if [ ${ano} = ${ANOF} ]  ; then mesc="01 02 03 04 05 06 07" ; fi
-	if [ ${ano} != ${ANOF} ] ; then mesc="01 02 03 04 05 06 07 08 09 10 11 12" ; fi
-	
+    if [ ${ano} != ${ANOF} ] ; then mesc="01 02 03 04 05 06 07 08 09 10 11 12" ; fi
     for mes in ${mesc}
     do
-	    echo ${ano}${mes}
-		# Calcula o índice eca_cdd.
+	echo ${ano}${mes}
+	# Calcula o índice eca_cdd.
         cdo -s eca_cdd,${R},${N} ${DIR_INPUT}/PREC.${ano}${mes}.nc ${DIR_TMP}/Brasil.eca_cdd.${ano}${mes}.nc
 	done
     for bioma in amazonia caatinga cerrado mata_atlantica pampa pantanal
     do
-	    echo ${ano} - ${bioma}
-		# Junta todos os meses (jan, ..., dez) para ficar separado por ano. 
-		cdo -s -O mergetime ${DIR_TMP}/Brasil.eca_cdd.${ano}??.nc ${DIR_OUTPUT}/Brasil.eca_cdd.${ano}.nc
-		# Interpola os biomas para a mesma resolução dos arquivos do eca_cdd.
-		cdo -s -remapbil,${ARQUIVO_MODELO} ${DIR_SHAPEFILE}/${bioma}.nc ${DIR_TMP}/mask.nc
-		# Aplica a máscara para cada bioma.
-		cdo -s -O ifnotthen ${DIR_TMP}/mask.nc ${DIR_OUTPUT}/Brasil.eca_cdd.${ano}.nc ${DIR_OUTPUT}/${bioma}.eca_cdd.${ano}.nc
-		# Como são duas variáveis, elas são separadas.
-		cdo -s splitvar ${DIR_OUTPUT}/${bioma}.eca_cdd.${ano}.nc ${DIR_TMP}/${bioma}.eca_cdd.${ano}.
-		# Salva a variável: consecutive_dry_days_index_per_time_period.
-		cdo -s output -nint -fldmean ${DIR_TMP}/${bioma}.eca_cdd.${ano}.consecutive_dry_days_index_per_time_period.nc | xargs > ${DIR_TMP}/${bioma}.eca_cdd.${ano}.consecutive_dry_days_index_per_time_period.txt
-		# Salva a variável: number_of_cdd_periods_with_more_than_${N}days_per_time_period.
-		cdo -s output -nint -fldmean ${DIR_TMP}/${bioma}.eca_cdd.${ano}.number_of_cdd_periods_with_more_than_${N}days_per_time_period.nc | xargs > ${DIR_TMP}/${bioma}.eca_cdd.${ano}.number_of_cdd_periods_with_more_than_${N}days_per_time_period.txt
+	echo ${ano} - ${bioma}
+	# Junta todos os meses (jan, ..., dez) para ficar separado por ano. 
+	cdo -s -O mergetime ${DIR_TMP}/Brasil.eca_cdd.${ano}??.nc ${DIR_OUTPUT}/Brasil.eca_cdd.${ano}.nc
+	# Interpola os biomas para a mesma resolução dos arquivos do eca_cdd.
+	cdo -s -remapbil,${ARQUIVO_MODELO} ${DIR_SHAPEFILE}/${bioma}.nc ${DIR_TMP}/mask.nc
+	# Aplica a máscara para cada bioma.
+	cdo -s -O ifnotthen ${DIR_TMP}/mask.nc ${DIR_OUTPUT}/Brasil.eca_cdd.${ano}.nc ${DIR_OUTPUT}/${bioma}.eca_cdd.${ano}.nc
+	# Como são duas variáveis, elas são separadas.
+	cdo -s splitvar ${DIR_OUTPUT}/${bioma}.eca_cdd.${ano}.nc ${DIR_TMP}/${bioma}.eca_cdd.${ano}.
+	# Salva a variável: consecutive_dry_days_index_per_time_period.
+	cdo -s output -nint -fldmean ${DIR_TMP}/${bioma}.eca_cdd.${ano}.consecutive_dry_days_index_per_time_period.nc | xargs > ${DIR_TMP}/${bioma}.eca_cdd.${ano}.consecutive_dry_days_index_per_time_period.txt
+	# Salva a variável: number_of_cdd_periods_with_more_than_${N}days_per_time_period.
+	cdo -s output -nint -fldmean ${DIR_TMP}/${bioma}.eca_cdd.${ano}.number_of_cdd_periods_with_more_than_${N}days_per_time_period.nc | xargs > ${DIR_TMP}/${bioma}.eca_cdd.${ano}.number_of_cdd_periods_with_more_than_${N}days_per_time_period.txt
     done
 done
 
@@ -60,13 +59,13 @@ for bioma in amazonia caatinga cerrado mata_atlantica pampa pantanal
 do
     for ano in $(seq 2001 2020)    
     do
-	    # Salva no formato texto as duas saídas do índice, converte de coluna para linha e adiciona ";" como delimitador.
-	    cat ${DIR_TMP}/${bioma}.eca_cdd.${ano}.consecutive_dry_days_index_per_time_period.txt | xargs | tr -s ' ' '; ' >> ${DIR_OUTPUT}/${bioma}.eca_cdd.R.txt
-	    cat ${DIR_TMP}/${bioma}.eca_cdd.${ano}.number_of_cdd_periods_with_more_than_${N}days_per_time_period.txt | xargs | tr -s ' ' '; ' >> ${DIR_OUTPUT}/${bioma}.eca_cdd.N.txt
-	done
+	# Salva no formato texto as duas saídas do índice, converte de coluna para linha e adiciona ";" como delimitador.
+	cat ${DIR_TMP}/${bioma}.eca_cdd.${ano}.consecutive_dry_days_index_per_time_period.txt | xargs | tr -s ' ' '; ' >> ${DIR_OUTPUT}/${bioma}.eca_cdd.R.txt
+	cat ${DIR_TMP}/${bioma}.eca_cdd.${ano}.number_of_cdd_periods_with_more_than_${N}days_per_time_period.txt | xargs | tr -s ' ' '; ' >> ${DIR_OUTPUT}/${bioma}.eca_cdd.N.txt
+    done
 done
 
-#rm -f ${DIR_TMP}/*.nc ${DIR_TMP}/*.txt
+rm -f ${DIR_TMP}/*.nc ${DIR_TMP}/*.txt
 
 #####################################
 datafinal=`date +%s`
