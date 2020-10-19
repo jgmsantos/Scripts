3941ef62e2c7b406b56751251007e62dac10e49b
Guilherme Martins <39347107+jgmsantos@users.noreply.github.com>
2020-10-19 16:05:51 -0300

    Add files via upload
    Script que extrai os valores a partir de uma lista de lat/lon.

---
+++ SHELL/07extrai.sh
@@ -0,0 +1,43 @@
+#/bin/bash 
+
+# Objetivo: Script que extrai os valores de uma latitude/longitude dada uma lista de coordenadas.
+# Autor: Guilherme Martins - 19/10/2020.
+
+# Crie um diretório com um nome qualquer, dentro desse diretório devem ter outros 3 diretórios:
+# input   => Contém os dados de entrada.
+# output  => É o diretório onde ficaram as saídas dos dados processados.
+# script  => O script "extrai.sh" e o arquivo "lat_lon.txt" devem ficar neste diretório.
+
+# Para executar o programa, basta digitar no terminal do Linux dentro do diretório script:
+# 1) chmod +x extrai.sh  => Torna o programa executável.
+# 2) ./extrai.sh         => Executa o script.
+
+input_latlon="lat_lon.txt" # Nome do arquivo de latitude e longitude. Esse arquivo (lat_lon.txt) TEM que ter apenas duas colunas e tem que ficar no diretório script.
+                           # A PRIMEIRA coluna é a LONGITUDE e a 
+				           # SEGUNDA coluna é a LATITUDE.
+
+lista_arquivos="prec.anual.nc nf.anual.nc"  # Lista de arquivos a serem processados. Aqui coloque o nome dos arquivos com a sua extensão.
+                                            # Como sugestão, faça assim ==> nomevariavel.nc, exemplo, prec.anual.nc; nf.anual.nc. Esse nome será utilizado para o 
+                                            # arquivo de saída no formato ".txt". A saída será assim => prec.anual.txt; nf.anual.txt.
+
+DIR_INPUT="../input"    # Caminho do diretório dos arquivos de entrada.
+DIR_OUTPUT="../output"  # Caminho do diretório dos arquivos processados.
+
+for nome_arquivo in ${lista_arquivos}
+do
+      
+	  echo "processando o arquivo: $nome_arquivo"
+	  
+      while read linha ; do
+
+            lon=$(echo ${linha} | awk {'print $1'})  # Pega apenas os valores de longitude.
+            lat=$(echo ${linha} | awk {'print $2'})  # Pega apenas os valores de latitude.
+            nome_variavel=$(echo ${nome_arquivo} | cut -d / -f3 | sed 's/.nc//g')  # Pega o nome da variável. Exemplo, o nome é prec.anual.nc, será armazenado apenas o nome "prec.anual".
+
+            echo "${nome_variavel} - Lat: ${lat} - Lon: ${lon}"
+
+            # O arquivo de saída terá 3 colunas: latitude longitude valor.
+            cdo -s -outputtab,nohead,date,lat,lon,value -remapnn,lon="$lon"_lat="$lat" ${DIR_INPUT}/${nome_arquivo} > ${DIR_OUTPUT}/${nome_variavel}.txt
+
+      done < ${input_latlon}
+done
