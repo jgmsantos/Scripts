d76e6b460d637dbe863bd3ae19253f132962fd6a
Guilherme Martins <jgmsantos@gmail.com>
2023-12-20 15:33:06 -0300

    Umidade relativa

--- CDO/calcula_ur.sh
+++ CDO/calcula_ur.sh
@@ -7,17 +7,29 @@
 # Referência: https://drive.google.com/file/d/15UT8bdlLlwxwazTvRk2IK28oys89Z4nl/view
 # Página: 78, item: 7.18 Calculando a Umidade Relativa.
 
-DIR_INPUT="../processado/diario"  # Diretório dos arquivos de entrada.
-DIR_TMP="../tmp"  # Diretório para processamento temporário.
-NOME_VARIAVEL_TD="d2m"  # Nome da variável do arquivo referente a Temperatura do Ponto de Orvalho a 2 metros.
-NOME_VARIAVEL_TT="t2m"  # Nome da variável do arquivo referente a Temperatura a 2 metros.
+DIR_INPUT="../input/T_Td_2m_horario"
+DIR_TMP="../tmp"
+DIR_OUTPUT="../output/UR"
 
-for ano in {1980..2021}
+rm -f ${DIR_TMP}/script.sh
+
+for ano in {2003..2022}
 do
-     echo $ano
-     cdo -s expr,'e = 6.1078 * 10^( (7.5*'$NOME_VARIAVEL_TD') / (237.3+'$NOME_VARIAVEL_TD') )' $DIR_INPUT/ERA5.d2m.$ano.nc $DIR_TMP/e.nc
-     cdo -s expr,'es = 6.1078 * 10^( (7.5*'$NOME_VARIAVEL_TT') / (237.3+'$NOME_VARIAVEL_TT') )' $DIR_INPUT/ERA5.t2m.$ano.nc $DIR_TMP/es.nc
-     cdo -s -O merge $DIR_TMP/e.nc $DIR_TMP/es.nc $DIR_TMP/e.es.nc
-     cdo -s -setattribute,rh2m@units="%" -setattribute,rh2m@long_name="Umidade Relativa a 2 metros" -expr,'rh2m=(e/es)*100.0' $DIR_TMP/e.es.nc $DIR_INPUT/ERA5.rh2m.${ano}.nc
-     rm -f ../tmp/e.nc ../tmp/es.nc ../tmp/e.es.nc  # Remove arquivos desnecessários.
+     echo "Processando: ${ano}"
+     cmd="cdo -s --no_history -b F32 -subc,273.15 -daymean ${DIR_INPUT}/ERA5_Superficie_T_Td_horario_2m_${ano}.nc ${DIR_TMP}/tmp01_${ano}.nc"
+     cmd="${cmd} && cdo -s --no_history expr,'e=6.1078*10^((7.5*d2m)/(237.3+d2m)) ; es=6.1078*10^((7.5*t2m)/(237.3+t2m))' ${DIR_TMP}/tmp01_${ano}.nc ${DIR_TMP}/tmp02_${ano}.nc"
+     cmd="${cmd} && cdo -s --no_history -setmissval,-999 -setattribute,rh@units='%' -setattribute,rh@long_name='Umidade Relativa a 2 metros' -expr,'rh=(e/es)*100' ${DIR_TMP}/tmp02_${ano}.nc ${DIR_OUTPUT}/RH_Diario_${ano}.nc"
+     echo ${cmd} >> ${DIR_TMP}/script.sh
 done
+
+# Executa em paralelo em blocos de 4 arquivos.
+parallel -j 4 < ${DIR_TMP}/script.sh
+
+# Junta todos os arquivos em um único arquivão com a umidade relativa diária.
+cdo -s -O --no_history -mergetime ${DIR_OUTPUT}/RH_Diario_????.nc ${DIR_OUTPUT}/RH_Diaria.nc
+
+# Junta todos os arquivos em um único arquivão e faz a média mensal.
+cdo -s -O --no_history -monmean ${DIR_OUTPUT}/RH_Diaria.nc ${DIR_OUTPUT}/RH_Mensal.nc
+
+# Limpa o diretório.
+rm -f ${DIR_TMP}/*
